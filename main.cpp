b611cce90871dd60506ce6fe5045449316afcc68
clem <clemens.cords@aol.com>
2022-05-01 00:30:56 +0200

    setup and working

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+// 
+// Copyright 2022 Clemens Cords
+// Created on 01.05.22 by clem (mail@clemens-cords.com)
+//
+
+#include <jluna.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    jluna::initialize();
+    jluna::Base["println"]("hello julia");
+
     return 0;
 }
+
